8a912323935e1d0bd1ab92348ad0e8f4daf14970
Jake Ross <jirhiker@gmail.com>
2022-03-04 15:21:42 -0700

    Initial commit

---
+++ static/map.js
@@ -0,0 +1,7 @@
+var map = L.map('map',
+    {preferCanvas: true}).setView([34.359593, -106.906871], 7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+

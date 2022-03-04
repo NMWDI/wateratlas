dd0918db4de5f60fc46225a320ec814fd7b4b144
Jake Ross <jirhiker@gmail.com>
2022-03-04 15:46:23 -0700

    started metrics table

--- main.py
+++ main.py
@@ -5,6 +5,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+DEBUG = False
+ENDPOINTS = {'st2': 'https://st2.newmexicowaterdata.org/FROST-Server/v1.1',
+             'nmenv': 'https://nmenv.newmexicowaterdata.org/FROST-Server/v1.1'}
+
+def get_count(url, tag):
+    url = f'{url}/{tag}?$top=1&$count=True'
+    resp = requests.get(url)
+    if resp:
+        return resp.json()['@iot.count']
+
+
+def make_endpoint_stats(key):
+    url = ENDPOINTS[key]
+
+    return {tag: get_count(url, tag) for tag in ('Locations',
+                                                 'Things',
+                                                 'Datastreams',
+                                                 'Observations')}
+
+
+def make_live_stats():
+    return [{'source': 'ST2',
+             'counts': make_endpoint_stats('st2')},
+            {'source': 'NMENV',
+             'counts': make_endpoint_stats('nmenv')}]
+
 
 @app.route('/')
 def root():
@@ -21,8 +47,16 @@ def root():
     markers = [{'coordinates': [l['location']['coordinates'][1], l['location']['coordinates'][0]],
                 'options': {'color': 'green', 'radius': 5}
                 } for l in locations]
+    if DEBUG:
+        live_stats = [{'source': 'ST2',
+                       'counts': {'Locations': 9102, 'Things': 9102, 'Datastreams': 13747, 'Observations': 1069871}},
+                      {'source': 'NMENV', 'counts': {'Locations': 53619, 'Things': 53619, 'Datastreams': 739588, 'Observations': 2832124}}]
+    else:
+        live_stats = make_live_stats()
 
-    return render_template('index.html', markers=markers)
+    return render_template('index.html',
+                           live_stats=live_stats,
+                           markers=markers)
 
 
 if __name__ == '__main__':
